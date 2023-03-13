8930b468408f82dd4a037f3342b28c329d23cc6f
Noah <noah.lund.syrdal@gmail.com>
2023-03-13 18:43:36 +0100

    13.03.18:42

--- src/main/java/java/Grid.java
+++ src/main/java/java/Grid.java
@@ -5,4 +5,17 @@ import java.util.ArrayList;
 public class Grid {
     private ArrayList<ArrayList<Character>> grid = new ArrayList<>();
     private Solution solution;
+
+    public Grid(GridGenerator generator){
+        ArrayListgrid = generator.gridGenerator(solution);
+    }
+
+    public void highlighter(String guess){
+        for(int i = 0; i<solution.size();i++){
+            String word = solution.getWord(i);
+            if(guess.equals(word)){
+                
+            }
+        }
+    }
 }
--- src/main/java/java/Solution.java
+++ src/main/java/java/Solution.java
@@ -12,5 +12,11 @@ public class Solution {
     public ArrayList<String> getSolution() {
         return solution;
     }
+    public int size(){
+        return solution.size();
+    }
+    public String getWord(int i){
+        return solution.get(i);
+    }
     
 }
