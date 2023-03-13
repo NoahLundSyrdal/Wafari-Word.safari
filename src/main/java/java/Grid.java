3d7f4464d6051918389870714c3e9af827f0d62f
Noah <noah.lund.syrdal@gmail.com>
2023-03-13 18:44:12 +0100

    13.03.18:44

--- src/main/java/java/Grid.java
+++ src/main/java/java/Grid.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class Grid {
     private ArrayList<ArrayList<Character>> grid = new ArrayList<>();
     private Solution solution;
+    private int points;
+    private int amount;
 
     public Grid(GridGenerator generator){
         ArrayListgrid = generator.gridGenerator(solution);
@@ -14,7 +16,9 @@ public class Grid {
         for(int i = 0; i<solution.size();i++){
             String word = solution.getWord(i);
             if(guess.equals(word)){
+                points++;
                 
+
             }
         }
     }
