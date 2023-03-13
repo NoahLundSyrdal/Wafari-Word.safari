f91d5a8c72e7c1e9870771d9d849832c65b40090
Matias Kim <matiakim@online.no>
2023-03-13 17:44:30 +0100

    V.3

--- src/main/java/java/GridGenerator.java
+++ src/main/java/java/GridGenerator.java
@@ -1,4 +1,18 @@
 package java;
+
+import java.util.ArrayList;
+
 public class GridGenerator {
-    
+    ArrayList<ArrayList<Character>> grid;
+    Solution solution
+
+    public gridGenerator(Solution solution) {
+        this.solution = solution;
+    }
+
+    public generate() {
+        
+    }
+
 }
+ 
