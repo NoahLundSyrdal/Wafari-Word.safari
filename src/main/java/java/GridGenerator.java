5eaa084af913bade2b3c1e646ded75dfe1ab6e61
Matias Kim <matiakim@online.no>
2023-03-14 09:32:29 +0100

    v10

--- src/main/java/java/GridGenerator.java
+++ src/main/java/java/GridGenerator.java
@@ -1,17 +1,56 @@
 package java;
 
 import java.util.ArrayList;
+import java.util.Random;
+
 
 public class GridGenerator {
-    ArrayList<ArrayList<Character>> grid;
-    Solution solution
+    private char[][] grid;
+    private Solution solution;
+    private String[] horizontal;
+    private String[] vertical;
 
-    public gridGenerator(Solution solution) {
+    public GridGenerator(Solution solution) {
         this.solution = solution;
     }
 
-    public generate() {
-        
+    public void generateHorizontalVertical() {
+        Random random = new Random();
+        int horizontalSize = random.nextInt(this.solution.size()/2);
+        int verticalSize= random.nextInt(this.solution.size() - horizontalSize);
+
+        horizontal = new String[horizontalSize];
+        vertical = new String[verticalSize];
+
+        for (int i = 0; i < horizontalSize; i++) {
+            horizontal[i] = this.solution.getWord(i);
+        }
+        for (int i = 0; i < verticalSize; i++) {
+            vertical[i] = this.solution.getWord(i+horizontalSize);
+        }
+
+    }
+
+    public String[] getHorizontal() {
+        return horizontal;
+    }
+
+    public String[] getVertical() {
+        return vertical;
+    }
+
+    public void createGrid() {
+        this.grid = new char[10][10];
+    }
+    
+    public void setWord() {
+        if (horizontal.length > vertical.length) {
+
+        }
+
+        else {
+
+        }
     }
 
 }
