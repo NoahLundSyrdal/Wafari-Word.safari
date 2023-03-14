3ea7634b4db1b56a95f87b239b62290080685e4b
Matias Kim <matiakim@online.no>
2023-03-14 10:54:55 +0100

    v12

--- src/main/java/java/GridGenerator.java
+++ src/main/java/java/GridGenerator.java
@@ -7,50 +7,66 @@ import java.util.Random;
 public class GridGenerator {
     private char[][] grid;
     private Solution solution;
-    private String[] horizontal;
-    private String[] vertical;
+    private ArrayList<String> horizontal;
+    private ArrayList<String> vertical;
+    private Random random = new Random();
 
     public GridGenerator(Solution solution) {
         this.solution = solution;
     }
 
     public void generateHorizontalVertical() {
-        Random random = new Random();
         int horizontalSize = random.nextInt(this.solution.size()/2);
         int verticalSize= random.nextInt(this.solution.size() - horizontalSize);
 
-        horizontal = new String[horizontalSize];
-        vertical = new String[verticalSize];
+        horizontal = new ArrayList<String>();
+        vertical = new ArrayList<String>();
 
         for (int i = 0; i < horizontalSize; i++) {
-            horizontal[i] = this.solution.getWord(i);
+            horizontal.add(this.solution.getWord(i));
         }
         for (int i = 0; i < verticalSize; i++) {
-            vertical[i] = this.solution.getWord(i+horizontalSize);
+            vertical.add(this.solution.getWord(i+horizontalSize));
         }
 
     }
 
-    public String[] getHorizontal() {
+    public ArrayList<String> getHorizontal() {
         return horizontal;
     }
 
-    public String[] getVertical() {
+    public ArrayList<String> getVertical() {
         return vertical;
     }
 
     public void createGrid() {
-        this.grid = new char[10][10];
+        this.grid = new char[solution.getLongestWordSize()+2][solution.getLongestWordSize()+2];
+        
+        // Random random = new Random();
+        // for (int i = 0; i < this.grid.length; i ++) {
+        //     for (int j = 0; j < this.grid[0].length; j ++) {
+        //         grid[i][j] = (char)(random.nextInt(26) + 'a');
+        //     }
+        // }
     }
     
-    public void setWord() {
-        if (horizontal.length > vertical.length) {
-
-        }
+    public void setWords() {
+        int i;
+        int j;
+        
+        while (horizontal.size() > 0 && vertical.size() > 0) {
+            try {
+                int r = random.nextInt(vertical.size())
+                for (char c : horizontal.get(r)) {
+                    
+                }
+            }
+        
 
         else {
 
         }
+    }
     }
 
 }
