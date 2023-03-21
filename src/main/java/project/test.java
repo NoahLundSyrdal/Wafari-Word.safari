8f1426e9c451a320053010597643f13e0c8b403b
Matias Kim <matiakim@online.no>
2023-03-21 20:36:41 +0100

    ew

--- src/main/java/project/test.java
+++ src/main/java/project/test.java
@@ -6,7 +6,7 @@ public class test {
     public static void main(String[] args) {
 
 
-        Solution solution = new Solution("foods", 10);
+        Solution solution = new Solution("colors", 5);
         GridGenerator gridGenerator = new GridGenerator(solution);
 
         gridGenerator.gridCreate();
