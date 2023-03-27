1d42120767b0f27bd1250869d17e7cf49fa3f713
Noah <noah.lund.syrdal@gmail.com>
2023-03-27 16:33:00 +0200

    ojoojoj

--- src/main/java/project/ProjectApp.java
+++ src/main/java/project/ProjectApp.java
@@ -1,14 +1,23 @@
 package project;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class ProjectApp extends Application {
 
+public class ProjectApp extends Application {
+    ObservableList<String> optionsList = FXCollections.observableArrayList("animals","sports","foods","colors","countries","fruits","capitalCities","videoGames","footballTeams","mountains","rivers","easter","halloween","christmas","music","science","nature","landmarks","creatures","jobs","planets","languages","books","insects","artStyles","sportsEquipment","dogBreeds");
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("App.fxml"));
+    Parent root = loader.load();
+    ComboBox<String> comboBox = (ComboBox<String>) loader.getNamespace().get("MyComboBox");
+    comboBox.setItems(optionsList);
     public static void main(String[] args) {
         Application.launch(args);
     }
@@ -19,5 +28,6 @@ public class ProjectApp extends Application {
         primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("App.fxml"))));
         primaryStage.show();
     }
+    
 
 }
