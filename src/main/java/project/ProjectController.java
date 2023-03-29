03f57ae87296821f1118802e46993c6d94d41c79
Matias Kim <matiakim@online.no>
2023-03-29 17:52:51 +0200

    NyTT VINDU

--- src/main/java/project/ProjectController.java
+++ src/main/java/project/ProjectController.java
@@ -1,8 +1,13 @@
 package project;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
 
 public class ProjectController {
     public ComboBox<String> theme;
@@ -15,9 +20,14 @@ public class ProjectController {
         amount.getItems().addAll(1,2,3,4,5,6,7,8,9,10);
     }
 
-    public void buttonOnClick(){
-        Solution solution = new Solution(theme.getValue(), amount.getValue());
-        GridGenerator gridGenerator = new GridGenerator(solution);
+    public void buttonOnClick() throws IOException{
+        //Solution solution = new Solution(theme.getValue(), amount.getValue());
+        //GridGenerator gridGenerator = new GridGenerator(solution);
+        Stage newStage = (Stage)createButton.getScene().getWindow();
+        newStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("Game.fxml"))));
+        newStage.show();
+        
+
 } 
 
 
