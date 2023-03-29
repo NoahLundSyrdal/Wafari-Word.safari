aacdae2e707ac1cf04527a9907318197df9b882d
Noah <noah.lund.syrdal@gmail.com>
2023-03-29 16:29:09 +0200

    Combobxes complete

--- src/main/java/project/ProjectController.java
+++ src/main/java/project/ProjectController.java
@@ -4,12 +4,13 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 
 public class ProjectController {
-    public ComboBox<String> myComboBox;
+    public ComboBox<String> theme;
+    public ComboBox<Integer> amount;
 
     @FXML
     public void initialize() {
-        myComboBox.getItems().addAll("animals","sports","foods","colors","countries","fruits","capitalCities","videoGames","footballTeams","mountains","rivers","easter","halloween","christmas","music","science","nature","landmarks","creatures","jobs","planets","languages","books","insects","artStyles","sportsEquipment","dogBreeds");
-        
+        theme.getItems().addAll("animals","sports","foods","colors","countries","fruits","capitalCities","videoGames","footballTeams","mountains","rivers","easter","halloween","christmas","music","science","nature","landmarks","creatures","jobs","planets","languages","books","insects","artStyles","sportsEquipment","dogBreeds");
+        amount.getItems().addAll(1,2,3,4,5,6,7,8,9,10);
     }
 
 }
