a7df878ffe68667dd11c95a278ee519e9c3b214c
Matias Kim <matiakim@online.no>
2023-03-29 16:01:37 +0200

    Scenebuilder

--- src/main/java/project/ProjectController.java
+++ src/main/java/project/ProjectController.java
@@ -2,8 +2,6 @@ package project;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 
 public class ProjectController {
     public ComboBox<String> myComboBox;
--- src/main/java/project/test.java
+++ src/main/java/project/test.java
@@ -1,7 +1,5 @@
 package project;
 
-import java.util.ArrayList;
-import java.util.Collection;
 
 public class test{
     public static void main(String[] args) {
